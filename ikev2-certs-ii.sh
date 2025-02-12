e716a8a3766097d8d1ef25c1620389643da81b15
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-02-12 14:24:35 +0300

    Update ikev2-certs-ii.sh

--- ikev2-certs-ii.sh
+++ ikev2-certs-ii.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
 
-# Убираем проверку версии Ubuntu
-# Проверяем наличие необходимых команд
+# Проверка наличия lsb_release
 if ! [ -x "$(command -v lsb_release)" ]; then
-    echo -e "\033[31;7mThis script requires lsb_release command. Terminating.\e[0m"
+    echo -e "\033[31;7mThis script supports only Ubuntu. Terminating.\e[0m"
     exit 1
 fi
 
-# Настройки
-export REGION=GB
+# Получаем внешний IP адрес
 export IP=$(curl -s api.ipify.org)
 
-# Обновление системы
+# Устанавливаем регион
+export REGION=GB
+
+# Обновление пакетов
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 
-# Устанавливаем необходимые пакеты
+# Установка необходимых пакетов
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y install strongswan strongswan-plugin-eap-mschapv2 moreutils iptables-persistent
 
 #=========== 
-# CERTS (Сертификаты)
+# Создание сертификатов
 #=========== 
-
 mkdir vpn-certs
 cd vpn-certs
 
-# Генерация корневого ключа
 ipsec pki --gen --type rsa --size 4096 --outform pem > server-root-key.pem
 chmod 600 server-root-key.pem
 
-# Генерация корневого сертификата
 ipsec pki --self --ca --lifetime 3650 \
 --in server-root-key.pem \
 --type rsa --dn "C=${REGION}, O=VPN Server, CN=VPN Server Root CA" \
 --outform pem > server-root-ca.pem
 
-# Генерация ключа для VPN сервера
 ipsec pki --gen --type rsa --size 4096 --outform pem > vpn-server-key.pem
 
-# Генерация сертификата для VPN сервера
 ipsec pki --pub --in vpn-server-key.pem \
 --type rsa | ipsec pki --issue --lifetime 1825 \
 --cacert server-root-ca.pem \
@@ -50,20 +46,18 @@ ipsec pki --pub --in vpn-server-key.pem \
 --flag serverAuth --flag ikeIntermediate \
 --outform pem > vpn-server-cert.pem
 
-# Копируем сертификаты в нужную директорию
 cp ./vpn-server-cert.pem /etc/ipsec.d/certs/vpn-server-cert.pem
 cp ./vpn-server-key.pem /etc/ipsec.d/private/vpn-server-key.pem
 
-# Устанавливаем права на ключ
 chown root /etc/ipsec.d/private/vpn-server-key.pem
 chgrp root /etc/ipsec.d/private/vpn-server-key.pem
 chmod 600 /etc/ipsec.d/private/vpn-server-key.pem
 
 #=========== 
-# STRONG SWAN CONFIG
+# Конфигурация StrongSwan
 #===========
 
-# Создаём /etc/ipsec.conf
+# Создание /etc/ipsec.conf
 cat << EOF > /etc/ipsec.conf
 config setup
     charondebug="ike 1, knl 1, cfg 0"
@@ -82,7 +76,7 @@ conn ikev2-vpn
     dpddelay=300s
     rekey=no
     left=%any
-    leftid=@server_name_or_ip
+    leftid=@${IP}
     leftcert=/etc/ipsec.d/certs/vpn-server-cert.pem
     leftsendcert=always
     leftsubnet=0.0.0.0/0
@@ -95,23 +89,18 @@ conn ikev2-vpn
     eap_identity=%identity
 EOF
 
-# Заменяем имя сервера или IP
-sed -i "s/@server_name_or_ip/${IP}/g" /etc/ipsec.conf
-
-# Добавляем секреты в /etc/ipsec.secrets
+# Создание /etc/ipsec.secrets
 cat << EOF > /etc/ipsec.secrets
 ${IP} : RSA "/etc/ipsec.d/private/vpn-server-key.pem"
-pegakmop %any% : EAP "vpnconfiguration"
+your_username %any% : EAP "your_password"
 EOF
 
 #=========== 
 # IPTABLES + FIREWALL
 #=========== 
 
-# Отключаем UFW (если он включён)
+# Отключаем UFW, если он был включен
 ufw disable
-
-# Устанавливаем политику по умолчанию
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -F
@@ -121,33 +110,29 @@ iptables -Z
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# Локальный трафик
+# Правила для loopback
 iptables -A INPUT -i lo -j ACCEPT
 
-# Правила для ipsec
-iptables -A INPUT -p udp --dport 500 -j ACCEPT
+# Правила для IPsec
+iptables -A INPUT -p udp --dport  500 -j ACCEPT
 iptables -A INPUT -p udp --dport 4500 -j ACCEPT
 
-# Настройки для маршрутизации
-iptables -A FORWARD --match policy --pol ipsec --dir in --proto esp -s 10.10.10.10/24 -j ACCEPT
+iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s 10.10.10.10/24 -j ACCEPT
 iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.10.10.10/24 -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -j MASQUERADE
 iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
-# Блокируем остальной трафик
 iptables -A INPUT -j DROP
 iptables -A FORWARD -j DROP
 
-# Сохраняем настройки
 netfilter-persistent save
 netfilter-persistent reload
 
 #=======
-# CHANGES TO SYSCTL (/etc/sysctl.conf)
+# Изменения в sysctl.conf
 #=======
 
-# Настройка параметров sysctl
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/" /etc/sysctl.conf
 sed -i "s/#net.ipv4.conf.all.accept_redirects = 0/net.ipv4.conf.all.accept_redirects = 0/" /etc/sysctl.conf
 sed -i "s/#net.ipv4.conf.all.send_redirects = 0/net.ipv4.conf.all.send_redirects = 0/" /etc/sysctl.conf
@@ -155,11 +140,12 @@ echo "" >> /etc/sysctl.conf
 echo "net.ipv4.ip_no_pmtu_disc = 1" >> /etc/sysctl.conf
 
 #=======
-# REBOOT
+# Перезагрузка
 #=======
 
 echo ""
-echo "The system will now reboot, and your VPN server should be up and running."
+echo "Looks like the script has finished successfully."
+echo "The system will now be re-booted and your VPN server should be up and running right after that."
 echo ""
 
 reboot
